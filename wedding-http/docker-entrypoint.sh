0ce456801227987234b4a841943c3c3e4a327146
Hadrien de Cuzey <hadrien.de.cuzey@futurice.com>
2018-01-17 12:52:53 +0200

    First attempt to wedding website

---
+++ wedding-http/docker-entrypoint.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+set -e
+
+source .env
+export SECRET_KEY_BASE=$WEDDING_SECRET_KEY_BASE
+export DROPBOX_ACCESS_TOKEN=$WEDDING_DROPBOX_ACCESS_TOKEN
+
+cat > database.yml <<EOF
+default: &default
+  adapter: mysql2
+  encoding: utf8
+  pool: 5
+  username: ${WEDDING_MYSQL_USER}
+  password: ${WEDDING_MYSQL_PASSWORD}
+  host: db
+  database: ${WEDDING_MYSQL_DATABASE}
+production:
+  <<: *default
+EOF
+
+bundle --path=vendor/bundle --clean
+bundle exec rake db:migrate
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
+
+exec "$@"
